08f333329707abc89756777339767ba79ee65cab
dantasaline <alinedantasdl@gmail.com>
2024-03-07 11:54:44 -0300

    arrumei a calculadora

--- Aula04/js/script.js
+++ Aula04/js/script.js
@@ -2,12 +2,14 @@ var altura = document.getElementById('altura')
 var peso = document.getElementById('peso')
 var resultado = document.getElementById('resultado')
 var diag = document.getElementById('diag')
+var sexo = document.getElementById("sexo").value
 
 function calculo(){
     var altura = Number(document.getElementById('altura').value)
     var peso = Number(document.getElementById('peso').value)
     var conta = peso/(altura^2)
     resultado.innerHTML = conta
+    diag.innerHTML = classIMC(sexo, conta)
     console.log('Resultado da operação é: ', conta)
 }
 
@@ -17,3 +19,41 @@ function limp(){
     document.getElementById('peso').value = ''
 }
 
+function classIMC(sexo, conta){
+    if(sexo = "masculino"){
+        return classmasc(conta)
+    }
+    else{
+        return classificarFeminino(conta)
+    }
+}
+
+function classmasc(conta){
+    if(conta<18.5){
+        return "Baixo peso"
+    }
+    else if(conta >= 18.5 && conta < 24.9){
+    return "Normal"
+    }
+    else if(conta >= 25 && conta < 29.9){
+    return "Sobrepeso"
+    }
+    else if(conta > 30){
+    return "Obesidade"
+    }
+}
+
+function classfem(conta){
+    if(conta<18.5){
+        return "Baixo peso"
+    }
+    else if(conta >= 18.5 && conta < 24.9){
+    return "Normal"
+    }
+    else if(conta >= 25 && conta < 29.9){
+    return "Sobrepeso"
+    }
+    else if(conta > 30){
+    return "Obesidade"
+    }
+}
