b1a446d146170d2e9943238eee5d77b2eb95539f
dantasaline <alinedantasdl@gmail.com>
2024-05-02 10:42:24 -0300

    adicionei botão de restaurar

--- Aula07 - Mario/js/script.js
+++ Aula07 - Mario/js/script.js
@@ -1,30 +1,38 @@
 $(document).ready(function() {
-    const mario = $('.mario');
-    const pipe = $('.pipe');
+    const mario = $('.mario')
+    const pipe = $('.pipe')
+    var restaura = $('#rest')
 
     const jump = () => {
-        mario.addClass('jump');
+        mario.addClass('jump')
 
         setTimeout(() => {
-            mario.removeClass('jump');
+            mario.removeClass('jump')
         }, 500);
     };
 
     const loop = setInterval(() => {
-        const pipePosit = pipe.offset().left;
-        const marioPosit = parseInt(mario.css('bottom').replace('px', ''));
+        const pipePosit = pipe.offset().left
+        const marioPosit = parseInt(mario.css('bottom').replace('px', ''))
 
         if (pipePosit <= 125 && pipePosit > 0 && marioPosit < 85) {
-            pipe.css('animation', 'none');
-            pipe.css('left', `${pipePosit}px`);
-            mario.css('animation', 'none');
-            mario.css('bottom', `${marioPosit}px`);
-            mario.attr('src', 'img/yoshim.webp');
-            mario.css('width', '135px');
-            mario.css('marginLeft', '50px');
-            clearInterval(loop);
+            pipe.css('animation', 'none')
+            pipe.css('left', `${pipePosit}px`)
+            mario.css('animation', 'none')
+            mario.css('bottom', `${marioPosit}px`)
+            mario.attr('src', 'img/yoshim.webp')
+            mario.css('width', '135px')
+            mario.css('marginLeft', '50px')
+            restaura.css('display', 'block')
+            clearInterval(loop)
         }
     }, 10);
 
-    $(document).keydown(jump);
+    $('#rest').click(function(){
+        location.reload()
+    })
+
+    $(document).keydown(jump)
+
+
 });
